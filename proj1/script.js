f3560c625e1cda1b44dc433a0397bbe1e3f4a163
Rishabh Jain <jainrishabh71011@gmail.com>
2022-02-10 16:58:22 +0530

    added on 10th Feb 2022

---
+++ proj1/script.js
@@ -0,0 +1,46 @@
+// var number = document.querySelector(".counter");
+
+
+var counter = document.querySelector(".counter");
+var followers = document.querySelector(".followers");
+
+// counter.innerHTML = 1001;
+
+
+let count = 1;
+setInterval(() =>{
+    if(count < 1000){
+        count++;
+    counter.innerText = count;
+    }
+},1)
+
+
+setTimeout(() => {
+    followers.innerText = "Followers on Instagram"
+},3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
