9e2a777d05ba431ec2920db331a36dc8bc34e1bb
Rishabh Jain <jainrishabh71011@gmail.com>
2022-02-10 20:49:36 +0530

    added on 10th Feb 2022

---
+++ proj2/scripts.js
@@ -0,0 +1,22 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet= document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+
+const center = document.querySelector(".center");
+
+// console.log(window.getComputedStyle(red).backgroundColor);
+
+// backgroundColor : #FFF;
+
+
+const getBGColor = (selectedElement) => {
+    return window.getComputedStyle(selectedElement).backgroundColor;
+};
+// console.log(getBGColor(orange));
+var color = getBGColor(pink);
+
+pink.addEventListener("click",() =>{
+    center.style.background = color;
+});
