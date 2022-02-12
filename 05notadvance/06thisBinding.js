9cd984c4685096772d1e359aa65804f9612e4325
Rishabh Jain <jainrishabh71011@gmail.com>
2022-02-12 11:00:36 +0530

    added on 12 th Feb 2022

--- 05notadvance/06thisBinding.js
+++ 05notadvance/06thisBinding.js
@@ -2,7 +2,7 @@ const hitesh = {
     firstName : "Rishabh",
     lastName : "Jain",
     role : "Admin",
-    courseCount = 3,
+    courseCount : 4,
     getInfo : function(){
         console.log(`
         First name is ${this.firstName}
@@ -11,4 +11,44 @@ const hitesh = {
         and he is studying ${this.courseCount} courses
         `);
     }
-}
+};
+// getInfo();
+
+const dj = {
+    firstName : "Rock",
+    lastName : "DJ",
+    role : "Sub-Admin",
+    courseCount : 4,
+};
+
+
+
+// hitesh.getInfo();
+// dj.getInfo();
+
+
+// getInfo : function(){
+//     console.log(`
+//     First name is ${this.firstName}
+//     Last name is ${this.lastName}
+//     His role is ${this.role}
+//     and he is studying ${this.courseCount} courses
+//     `);
+// }
+
+// var djInfo = hitesh.getInfo.bind(dj);
+// djInfo();
+
+
+hitesh.getInfo.call(dj);
+
+
+
+
+
+
+
+
+
+
+
